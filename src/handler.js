24f74e3404a23188f3e79db4b5a2d027018ed584
Hari Saputra <hari.saputra.dev@gmail.com>
2022-01-06 12:32:25 +0700

    versi 1.0

---
+++ src/handler.js
@@ -0,0 +1,69 @@
+const { nanoid } = require("nanoid");
+const notes = require("./notes");
+
+const addNotesHandler = (request, h) => {
+    const { title, tags, body } = request.payload;
+
+    const id = nanoid(16);
+    const createdAt = new Date().toISOString;
+    const updatedAt = createdAt;
+
+    const newNote = {
+        title, tags, body, createdAt, updatedAt,
+    };
+
+    notes.push(newNote);
+
+    const isSuccess = notes.filter((note) => note.id === id).length > 0;
+
+    if (isSuccess) {
+        const response = h.response({
+            status: 'success',
+            message: 'Note added successfully',
+            data: {
+                noteId: id,
+            },
+        });
+        response.code(201);
+        return response;
+    }
+
+    const response = h.response({
+        status: 'fail',
+        message: 'Note failed to add'
+    });
+
+    response.code(500);
+    return response;
+};
+
+const getAllNotesHandler = () => ({
+    status: 'success',
+    data: {
+        notes,
+    },
+});
+
+const getNoteByIdHandler = (request, h) => {
+    const { id } = request.params;
+    const note = notes.filter((n) => n.id === id)[0];
+
+    if (note !== undefined) {
+        return {
+            status: 'success',
+            data: {
+                note,
+            },
+        };
+    }
+
+    const response = h.response({
+        status: 'fail',
+        message: 'Note not found',
+    });
+
+    response.code(404);
+    return response;
+};
+
+module.exports = { addNotesHandler, getAllNotesHandler, getNoteByIdHandler };
