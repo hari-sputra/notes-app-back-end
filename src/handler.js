c9f515979432f7557ebde61771b2306f4969ab98
Hari Saputra <hari.saputra.dev@gmail.com>
2022-01-06 13:19:44 +0700

    update notes feature

--- src/handler.js
+++ src/handler.js
@@ -66,4 +66,37 @@ const getNoteByIdHandler = (request, h) => {
     return response;
 };
 
-module.exports = { addNotesHandler, getAllNotesHandler, getNoteByIdHandler };
+const editNoteByIdHandler = (request, h) => {
+    const { id } = request.params;
+    const { title, tags, body } = request.payload;
+    const updatedAt = new Date().toISOString;
+    const index = notes.findIndex((note) => note.id === id)
+
+    if (index !== -1) {
+        notes[index] = {
+            ...notes[index],
+            title,
+            tags,
+            body,
+            updatedAt,
+
+        };
+
+        const response = h.response({
+            status: 'success',
+            message: 'Note Updated successfully',
+        });
+        response.code(200);
+        return response;
+    }
+
+    const response = h.response({
+        status: 'fail',
+        message: 'Note failed to update. Id not found!',
+    });
+
+    response.code(404);
+    return response;
+};
+
+module.exports = { addNotesHandler, getAllNotesHandler, getNoteByIdHandler, editNoteByIdHandler };
